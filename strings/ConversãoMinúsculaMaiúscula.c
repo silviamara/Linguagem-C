018a15629235aa9d040f78b4ab1a1428a0048ecb
Sílvia Campos <129518227+silviamara@users.noreply.github.com>
2025-07-03 16:46:37 -0300

    Update ConversãoMinúsculaMaiúscula.c

--- strings/ConversãoMinúsculaMaiúscula.c
+++ strings/ConversãoMinúsculaMaiúscula.c
@@ -1,1 +1,34 @@
+/% Faça um programa que receba uma frase com letras minúsculas e 
+  converta a primeira letra de cada palavra para maiúscula. %/
+
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+	char frase[50];
+	int tamanho, i;
+	
+	printf("Entre com uma frase:\n");
+	scanf ("%[^\n]", frase);
+
+  //Converte todos os caracteres da frase para minúsculo
+	strlwr(frase);
+	tamanho = strlen(frase);
+
+  //Verificar o primeiro caractere da frase
+	if (frase[0]!= '\0'  || frase[0]!= ' ')
+		frase[0]=toupper(frase[0]);
+	
+	for (i=1; i<tamanho; i++) {
+		if (frase[i]==' ') {
+		    while (frase[i+1] == ' ')
+		       i = i+1;
+		    frase[i+1]=toupper(frase[i+1]);
+		}	
+	}
+	printf("\nNova frase = %s", frase);
+	return 0;
+}
 
